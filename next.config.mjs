95a3975e793068e9a995811dc9ae6eb8e1f8b2be
10292539 <raghav.10292539@gov.in>
2026-02-25 11:44:35 +0530

    updated code

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support Next.js default image optimization
+  },
+  // Replace 'your-repo-name' with your actual repository name
+  // basePath: '/your-repo-name',
 };
 
 export default nextConfig;
